568f9a207569c7ed9066bd39d22c82c9703da9d8
bajisapp@gmail.com <bajisapp@gmail.com>
2024-02-26 03:03:54 +0800

    first commit

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import reportWebVitals from './reportWebVitals';
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap-icons/font/bootstrap-icons.min.css"
 import "bootstrap/dist/js/bootstrap.js"
-import "bootstrap/dist/js/bootstrap.bundle.js"
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
